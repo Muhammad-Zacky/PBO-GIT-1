c01d6a1e9f1ce961370e659b02ff6bd5e6acdd2a
Muhammad Zacky <81474961+Muhammad-Zacky@users.noreply.github.com>
2024-03-20 09:49:03 +0700

    Create Latihan1.java
    -Latihan Segitiga

---
+++ Latihan1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+   
+class SegitigaBintang {
+  public static void main(String args[]){
+            
+    Scanner input = new Scanner(System.in);
+       
+    System.out.println("Program Segitigah");
+    System.out.println();
+     
+    int tsmz3019173222,kltwi1,kltwij;
+  
+    tinggi_segitiga = 5;
+     
+    System.out.println();
+    
+    for(kltwi1=1; kltwi1<=tsmz3019173222; kltwi1++) {
+      for(kltwij=1; kltwij<=kltwi1; kltwij++) {
+        System.out.print(" *");
+      }
+      System.out.println();
+    }
+     
+  }
+}
