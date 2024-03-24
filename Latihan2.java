51f35a00c864276ec0e1b1154ea90c37c1adac9b
TR0110 <81474961+Muhammad-Zacky@users.noreply.github.com>
2024-03-24 20:46:31 +0700

    Create Latihan2.java

---
+++ Latihan2.java
@@ -0,0 +1,32 @@
+public class Latihan2 {
+    
+    public static void main(String[] args) {
+          int i, j;
+                  for(i=0; i<5; i++){
+                    for(j=5; j>i; j--){
+                    
+                    if(j==5){
+                        System.out.print("a");
+                    }
+                    else if(j==4){
+                        System.out.print("b");
+                    }
+                    
+                    else if(j==3){
+                        System.out.print("c");
+                    }
+                    
+                    else if(j==2){
+                        System.out.print("d");
+                    }
+                    
+                    else if(j==1){
+                        System.out.print("e");
+                    }
+                
+                
+                    }System.out.println("");
+            }
+        }
+    
+}
